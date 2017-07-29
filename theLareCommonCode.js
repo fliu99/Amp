f15096721287124f8a634acdae0f29270926b794
Brooklyn Team <bteam@imac-azdhjw.wgenhq.net>
2017-07-29 16:49:51 -0400

    VR

---
+++ theLareCommonCode.js
@@ -0,0 +1,5 @@
+
+var vRViewPlayer = function(elementIdName, options) {
+  console.log('create more than one');
+  return new VRView.Player(elementIdName, options);
+}
---
+++ webVRDayTwo.js
@@ -0,0 +1,6 @@
+var vrHomePage = vRViewPlayer('#vr-homepage', {
+    image: 'IMG_20170729_160246.vr.jpg',
+    is_stereo: false,
+    is_autopan_off: true,
+});
+window.addEventListener('load', vrHomePage);
