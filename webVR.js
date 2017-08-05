f09559c82bff87e1a7707cdd94ef74b73031d6fc
Candidate <fake@email.com>
2017-08-05 14:55:46 -0400

    Hot

--- webVR.js
+++ webVR.js
@@ -3,3 +3,11 @@ var myPicture = vRViewPlayer('#vr', {
   is_stereo: false
 })
 window.addEventListener('load',myPicture);
+myPicture.on('ready', function(event) {
+  myPicture.addHotspot('creating-hotspot-1', {
+    pitch: 0,
+    yaw: 0,
+    radius: 0.10,
+    distance: 1
+  });
+});
