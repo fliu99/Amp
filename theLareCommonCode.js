4fcf94f284d8195e5addf0e7c2e7eddb19914df1
Candidate <fake@email.com>
2017-08-05 14:27:44 -0400

    debugggin

--- theLareCommonCode.js
+++ theLareCommonCode.js
@@ -1,4 +1,4 @@
-var vRViewPlayer = function("#vr", options) {
+var vRViewPlayer = function("#vr") {
 console.log('Creating VR View Player');
-return new VRView.Player("#vr", options);
+return new VRView.Player("#vr");
 }
