cdb8af27cb95aacf0664fc4540bc9b4c1bf08966
fliu99 <30555875+fliu99@users.noreply.github.com>
2017-08-12 14:23:06 -0400

    Add files via upload

--- webVR.js
+++ webVR.js
@@ -48,47 +48,45 @@ myPicture.on('click',function(event){
       height: '100%',
       width: '100%'
     });
-    window.addEventListener('load',myPicture);
-    myPicture.on('ready', function(event) {
-      myPicture.addHotspot('creating-hotspot-1', {
+      myPicture.addHotspot('creating-hotspot-1D', {
         pitch: 0,
         yaw: 15,
         radius: 0.10,
         distance: 1
       });
-      myPicture.addHotspot('hotspot2', {
+      myPicture.addHotspot('hotspot2D', {
         pitch: 0,
         yaw: 90,
         radius: 0.10,
         distance: 1
       });
-      myPicture.addHotspot('hotspot3', {
+      myPicture.addHotspot('hotspot3D', {
         pitch: 0,
         yaw: 45,
         radius: 0.10,
         distance: 1
       });
-      myPicture.addHotspot('hotspot4', {
+      myPicture.addHotspot('hotspot4D', {
         pitch: 0,
         yaw: -95,
         radius: 0.10,
         distance: 1
       });
-    });
+
     myPicture.on('click',function(event){
       console.log('register all click events');
-      if (event.id == 'creating-hotspot-1'){
+      if (event.id == 'creating-hotspot-1D'){
         alert('Here at the beach I relax and have fun on a hot day!');
       }
-      if (event.id == 'hotspot2'){
+      if (event.id == 'hotspot2D'){
         alert('I use the highway as my method of transportation in order to get to my destination quickly.')
       }
-      if (event.id == 'hotspot3'){
+      if (event.id == 'hotspot3D'){
         alert('The shopping center is where I buy all my daily necessities and things I want.')
       }
-      if (event.id == 'hotspot4'){
+      if (event.id == 'hotspot4D'){
         myPicture.setContent({
-          image: 'Image2.jpg',
+          image: 'Image.jpg',
           is_stereo: false,
           height: '100%',
           width: '100%'
